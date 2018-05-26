43d554709fb5a5144f8f80f6a0eed14e4b12e000
Nikita Besednyi <nikmac10x@gmail.com>
2018-05-26 14:36:51 +0300

    first

---
+++ app/src/main/java/ru/nikmac10x/smartservice/MainActivity.java
@@ -0,0 +1,15 @@
+package ru.nikmac10x.smartservice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sofia_kp.KPICore;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
